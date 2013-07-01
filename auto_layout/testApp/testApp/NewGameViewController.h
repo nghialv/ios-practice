6a02aa0eefbe6a8ff57e16cb1f5e42fb3835addb
Denny <dennycd@me.com>
2013-07-01 12:59:36 -0700

    completing practice

---
+++ auto_layout/testApp/testApp/NewGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewGameViewController.h
+//  testApp
+//
+//  Created by Denny C. Dai on 2013-07-01.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewGameViewController : UIViewController
+
+@end
---
+++ auto_layout/testApp/testApp/NewGameViewController.m
@@ -0,0 +1,38 @@
+//
+//  NewGameViewController.m
+//  testApp
+//
+//  Created by Denny C. Dai on 2013-07-01.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#import "NewGameViewController.h"
+
+@interface NewGameViewController ()
+
+@end
+
+@implementation NewGameViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
