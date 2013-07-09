1aeca790c0394692598f023b22c296fca6933c04
Denny C. Dai <dennycd@me.com>
2013-07-09 09:18:59 -0700

    schem work project

---
+++ xcode/SchemeWorkspace/README.md
@@ -0,0 +1,5 @@
+## working with schemes and projects in XCode 
+
+based from WWDC 2012 session #408 
+
+
---
+++ xcode/SchemeWorkspace/SchemeWorkspace/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SchemeWorkspace
+//
+//  Created by Denny C. Dai on 7/9/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
