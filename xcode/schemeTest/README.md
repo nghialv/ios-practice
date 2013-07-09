28ffedd97d02f106fb46eff4bfff9b106dba32f6
Denny C. Dai <dennycd@me.com>
2013-07-09 10:14:32 -0700

    initi imporot

---
+++ xcode/schemeTest/README.md
@@ -0,0 +1,14 @@
+## working with schemes and projects in XCode 
+
+based from WWDC 2012 session #408 
+
+
+### tips 
+
+* use opt-click on Run/Profile/Analyze/ to bring up scheme panel
+* find derived data directory from Preference => Location 
+* File ==> Workspace Setting to set per workspace/project derived data location
+* File ==> Workspace Setting => Advanced 
+
+
+* xcrun nm -a  /Path-to-Build-Project
---
+++ xcode/schemeTest/schemeTestiOS/schemeTestiOSTests/schemeTestiOSTests.m
@@ -0,0 +1,37 @@
+//
+//  schemeTestiOSTests.m
+//  schemeTestiOSTests
+//
+//  Created by Denny C. Dai on 7/9/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface schemeTestiOSTests : XCTestCase
+
+@end
+
+@implementation schemeTestiOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTAssertTrue(true, @"easy test");
+    //XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
