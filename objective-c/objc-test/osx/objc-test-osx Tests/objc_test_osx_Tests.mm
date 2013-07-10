2c6c4042217e6eb17f9163dedea020e4adbe9194
Denny C. Dai <dennycd@me.com>
2013-07-10 13:43:01 -0700

    unit testing for ARC in method returns

--- objective-c/objc-test/osx/objc-test-osx Tests/objc_test_osx_Tests.mm
+++ objective-c/objc-test/osx/objc-test-osx Tests/objc_test_osx_Tests.mm
@@ -8,7 +8,6 @@
 
 #import <XCTest/XCTest.h>
 #import <vector>
-
 #import "myObject.h"
 
 //struct with objc object pointer (Objective-C++)
@@ -19,12 +18,43 @@ struct MyStruct {
 };
 
 
+@protocol MyListProtocol <NSObject>
+@end
 
 //subscripting support
-@interface MyList : NSObject
+@interface MyList : NSObject{
+    myObject* _obj;
+}
+-(myObject*)getObj;
+-(myObject*)newObj;
+
+@property(weak) id<MyListProtocol> delegate; //delegate is weak so that it automaticall gets nil
+
++(void)singletonOp; //singleton operation declared as a class level method
 @end
 
 @implementation MyList
+
++(void)singletonOp{
+    
+    //to guide for thread safety, simply use dispatch_once
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSLog(@"dispatch once only!");
+    });
+    
+}
+
+//retain on the obj and just return
+-(myObject *)newObj{
+    return [[myObject alloc] initWithName:@"newobj"];
+}
+
+//retain _obj, then autorelease it and then return
+// this INCREASE the reference count of the obj by 1 !!!!!!
+-(myObject *)getObj{
+    return _obj;
+}
 
 //support index subscript
 - (myObject*)objectAtIndexedSubscript:(NSUInteger)idx{
@@ -34,6 +64,18 @@ struct MyStruct {
 //support key sbuscript
 - (myObject*)objectForKeyedSubscript:(id)key{
     return [[myObject alloc] initWithName:@"s"];
+}
+
+
+-(id)init{
+    if(self = [super init]){
+        _obj = [[myObject alloc] initWithName:@"dummy"];
+    }
+    return self;
+}
+
+-(void)dealloc{
+    NSLog(@"MyList::dealloc");
 }
 
 @end
@@ -60,7 +102,131 @@ struct MyStruct {
     [super tearDown];
 }
 
+-(void)testSingletonPattern{
+    [MyList singletonOp];
+}
 
+-(void)testMethodReturn {
+    
+    __weak myObject* weakObj;
+    
+    
+    {
+        MyList* list = [[MyList alloc] init];
+    
+        //the returned obj has ref count 1,
+        //localStorng copied it, ref count incre to 2
+        //then the returned local obj out of statement scope, its ref count go to 1
+        __strong myObject* localStrong = [list newObj];
+        
+        weakObj = localStrong; //does not retain count
+        
+        //now list is destroyed, along with its ivar
+        //the returned myobj decre count to 0, therefore destroyed
+    }
+    XCTAssert(weakObj == nil, @"");
+    
+
+    {
+        MyList* list = [[MyList alloc] init];
+        weakObj = [list getObj];
+    }
+    
+    //because of the extra retain while getting access,
+    //the local ivar lives beyond list's life cycle, but before next autorelease pool
+    //SO, someone need to fix this 
+    XCTAssert(weakObj != nil, @"");
+    
+    
+    //now using an explicit autorelease pool
+    weakObj = nil;
+    @autoreleasepool {
+        
+        {
+            MyList* list = [[MyList alloc] init];
+            weakObj = [list getObj];
+        }
+        
+        //because of the extra retain while getting access,
+        //the local ivar lives beyond list's life cycle, but before next autorelease pool
+        //SO, someone need to fix this
+        XCTAssert(weakObj != nil, @"");
+    }
+    
+    //now, autoreleae pool will purge the temporary obj with extra retain due to the access method
+    XCTAssert(weakObj==nil, @"");
+    
+}
+
+-(void)testLocalObj{
+    
+    __weak myObject* obj;
+    {
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+        
+        //so the lifecycle of the obj only lives within a single statement
+        obj = [[myObject alloc] initWithName:@"localvar"];
+        XCTAssert(obj==nil, @"");
+        
+#pragma clang diagnostic pop
+
+    }
+    
+    XCTAssert(obj==nil, @"");
+}
+
+
+
+/**
+  A total of four differenct ownerships
+  
+  - objective-c, block pointers 
+  - local, globals, parameters, instance variables
+ 
+ **/
+-(void)testReference{
+    
+    //all variables default strong
+    //like a retain property
+    //implicitly initialized to nil - safe for ARC-generated code
+    __strong myObject* obj;
+    XCTAssert(obj==nil, @"local var initlized to nil under ARC");
+    
+    
+    //reference things we don't want to own, but being able to reach
+    //becomes nil as soon as object starts dealloc
+    __weak myObject* weakObj;
+    
+    //like a traditional var, not initialized, no extra logic, no restriction,
+    //useful in global structs with constant @"" string
+    //__unsafe_unretained myObject* unsafeObj;
+    
+    {
+        myObject* obj1 = [[myObject alloc] initWithName:@"denny"]; //retain count is 1
+        
+        myObject* obj2;
+        
+        //we will increase the reference count of the  obj pointed to by str by 1
+        //then decrease existing obj reference count on str2 by 1, then copy over
+        obj2 = obj1;  //retain count to 2
+        
+        
+        weakObj = obj1;
+        XCTAssert(weakObj!=nil,@"");
+        
+        //when out of scope, local object gets reference count to zero, deallocated
+    }
+    
+    //not nessarily true, as it might not get deallocated yet
+    XCTAssert(weakObj==nil, @"a local obj is deallocated, reflected as nil in weak var");
+
+    
+    //when out of scope,
+    // current value is implicitly releases
+    NSLog(@"done");
+}
 
 
 -(void)testObjectLiteral{
