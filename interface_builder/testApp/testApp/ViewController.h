a2d607533539acaeaf5c7065982d8ba0f3db9981
Denny C. Dai <dennycd@me.com>
2013-07-16 16:02:32 -0700

    auto layout test mac os x source initial commit

---
+++ interface_builder/testApp/testApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testApp
+//
+//  Created by Denny C. Dai on 2013-07-01.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
