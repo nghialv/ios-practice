16292c025cad656c4ad4ddcc78f8610c81572d17
Denny C. Dai <dennycd@me.com>
2013-07-19 15:37:46 -0700

    intial commit of general app practice proj

---
+++ general/GeneralApp/GeneralApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GeneralApp
+//
+//  Created by Denny C. Dai on 7/19/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
