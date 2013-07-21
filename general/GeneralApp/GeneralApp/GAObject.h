17bad94cc7ec15855e2107e1e6c9ca875fa44b7d
Denny C. Dai <dennycd@me.com>
2013-07-20 22:27:29 -0700

    sample data model object

---
+++ general/GeneralApp/GeneralApp/GAObject.h
@@ -0,0 +1,16 @@
+//
+//  GAObject.h
+//  GeneralApp
+//
+//  Created by Denny C. Dai on 7/20/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+  A Sample Data Model Object
+ **/
+@interface GAObject : NSObject
+
+@end
---
+++ general/GeneralApp/GeneralApp/GAObject.m
@@ -0,0 +1,19 @@
+//
+//  GAObject.m
+//  GeneralApp
+//
+//  Created by Denny C. Dai on 7/20/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#import "GAObject.h"
+
+//ivars and hidden properties goes here 
+@interface GAObject(){
+    
+}
+@end
+
+@implementation GAObject
+
+@end
