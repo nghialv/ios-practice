796f17e69d3797e497eaf3dc34faeebf180a31b7
Denny C. Dai <dennycd@me.com>
2013-08-07 16:39:20 -0700

    objc object basics

--- objective-c/objc-test/ios/objc-test/TestObject.h
+++ objective-c/objc-test/ios/objc-test/TestObject.h
@@ -8,8 +8,27 @@
 
 #import <Foundation/Foundation.h>
 
-@interface TestObject : NSObject
+//A protocol declares methods that can be implemented by any class. Protocols are not classes themselves. They simply define an interface that other objects are responsible for implementing. When you implement the methods of a protocol in one of your classes, your class is said to conform to that protocol.
+@protocol MyProtocol <NSObject>
+@required
+-(void)myFunc;
+@optional
+-(void)myOptFunc;
+@end
+
+@interface TestObject : NSObject <MyProtocol>
+
+//Declared properties are a convenience notation used to replace the declaration and, optionally, implementation of accessor methods.
+//Practically speaking, properties reduce the amount of redundant code you have to write.
 @property(nonatomic,strong) NSString* name;
+
+
 -(instancetype)initWithName:(NSString*)name;
--(void)foo;
+-(void)foo; //instance method
++(void)bar; //class method
+
+
+//manual definition of getter/setter
+-(NSInteger)value;
+-(void)setValue:(NSInteger)val;
 @end
--- objective-c/objc-test/ios/objc-test/TestObject.m
+++ objective-c/objc-test/ios/objc-test/TestObject.m
@@ -9,21 +9,53 @@
 #import "TestObject.h"
 
 @interface TestObject(){
-    id _ivar1;
-    id _ivar2;
+    id _ivar1;  //instance variables a.k.a ivars
+    id _ivar2;  //weak typing
+    NSString* myname; //strong typing with explicit class name
+    
+    NSInteger _value;
 }
 @end
 
 @implementation TestObject
-    
--(void)foo{
-    
 
+#pragma mark - MyProtocol
+-(void)myFunc
+{
+    NSLog(@"TestObject::myFunc");
+}
+
+#pragma mark  -
+
+-(NSInteger)value
+{
+    return _value;
+}
+
+
+-(void)setValue:(NSInteger)val
+{
+    _value = val;
+}
+
+
+/**
+ When you want to call a method, you do so by messaging an object. A message is the method signature, along with the parameter information the method needs. All messages you send to an object are dispatched dynamically, thus facilitating the polymorphic behavior of Objective-C classes.
+ **/
+-(void)foo{
+    NSLog(@"foo");
+}
+
+
++(void)bar
+{
+    NSLog(@"bar");
 }
 
 -(instancetype)initWithName:(NSString*)name{
     if(self = [super init]){
         _name = name;
+        _value = 11;
     }
     return self;
 }
