b947f16db6bb0b4575f6b9cfce0ffe08fed137ed
Denny C. Dai <dennycd@me.com>
2013-08-12 20:28:42 -0700

    c++ to objC reference test case

---
+++ objective-c/objc-test/ios/MyCppCls.h
@@ -0,0 +1,22 @@
+//
+//  MyCppCls.h
+//  objc-test
+//
+//  Created by Denny C. Dai on 8/12/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#ifndef __objc_test__MyCppCls__
+#define __objc_test__MyCppCls__
+
+#include <iostream>
+
+class MyCppCls {
+public:
+    MyCppCls(id orign);
+    
+private:
+    id __weak m_id; //avoid strong reference that creates a retain cycle with an objective c object
+};
+
+#endif /* defined(__objc_test__MyCppCls__) */
---
+++ objective-c/objc-test/ios/MyCppCls.mm
@@ -0,0 +1,14 @@
+//
+//  MyCppCls.cpp
+//  objc-test
+//
+//  Created by Denny C. Dai on 8/12/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#include "MyCppCls.h"
+
+
+MyCppCls::MyCppCls(id origin){
+    m_id = origin;
+}
