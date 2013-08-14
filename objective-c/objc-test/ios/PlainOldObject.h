7c29435fad82a0844933b7197cff1ad2710720c2
Denny C. Dai <dennycd@me.com>
2013-08-14 14:42:21 -0700

    objC class without inheriting from NSObject

---
+++ objective-c/objc-test/ios/PlainOldObject.h
@@ -0,0 +1,16 @@
+//
+//  PlainOldObject.h
+//  objc-test
+//
+//  Created by Denny C. Dai on 8/14/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+  will get compiler error if not inheriting from NSObject
+ **/
+@interface PlainOldObject : NSObject
+
+@end
---
+++ objective-c/objc-test/ios/PlainOldObject.m
@@ -0,0 +1,13 @@
+//
+//  PlainOldObject.m
+//  objc-test
+//
+//  Created by Denny C. Dai on 8/14/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#import "PlainOldObject.h"
+
+@implementation PlainOldObject
+
+@end
