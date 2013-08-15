2ae82595c69f9b61a4f8bc5be47d134b025eda8b
Denny C. Dai <dennycd@me.com>
2013-08-15 11:38:11 -0700

    IBOutlet design pattern test with an object and its outlet property both stored in nib file

---
+++ design_pattern/testIOS/testIOS/dpObject.h
@@ -0,0 +1,26 @@
+//
+//  dpObject.h
+//  testIOS
+//
+//  Created by Denny C. Dai on 8/15/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface dpObject : NSObject
+
+/**
+  REF https://developer.apple.com/library/ios/documentation/General/Conceptual/CocoaEncyclopedia/Outlets/Outlets.html#//apple_ref/doc/uid/TP40010810-CH10-SW1
+ 
+ The connections between the containing object and its outlets are reestablished every time the containing object is unarchived from its nib file.
+ 
+ 
+ An application typically sets outlet connections between its custom controller objects and objects on the user interface, but they can be made between any objects that can be represented as instances in Interface Builder, even between two custom objects.
+ 
+ Check  testIB.xib for thw tow object dpObject and keywords
+ 
+ an instance of dpObject and NSArray is stored in a nib file, when unarchived they are both instantiated and connection established in-between them
+ **/
+@property(weak) IBOutlet NSArray* keywords;
+@end
---
+++ design_pattern/testIOS/testIOS/dpObject.m
@@ -0,0 +1,13 @@
+//
+//  dpObject.m
+//  testIOS
+//
+//  Created by Denny C. Dai on 8/15/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#import "dpObject.h"
+
+@implementation dpObject
+
+@end
