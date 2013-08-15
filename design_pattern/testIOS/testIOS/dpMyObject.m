95a809212f0eb8221bf2ecf28edd116e6ceb49c0
Denny C. Dai <dennycd@me.com>
2013-08-15 16:13:26 -0700

    objc subclassing test

---
+++ design_pattern/testIOS/testIOS/dpMyObject.m
@@ -0,0 +1,13 @@
+//
+//  dpMyObject.m
+//  testIOS
+//
+//  Created by Denny C. Dai on 8/15/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#import "dpMyObject.h"
+
+@implementation dpMyObject
+
+@end
