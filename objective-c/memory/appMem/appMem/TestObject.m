24f7448e6992ab7bfdc670c011082c5f23bf9a38
Denny C. Dai <dennycd@me.com>
2013-08-19 15:28:37 -0700

    dealloc under ARC

--- objective-c/memory/appMem/appMem/TestObject.m
+++ objective-c/memory/appMem/appMem/TestObject.m
@@ -16,6 +16,13 @@
  Custom dealloc methods in ARC do not require a call to [super dealloc] (it actually results in a compiler error). The chaining to super is automated and enforced by the compiler.
  
  **/
+
+//Because ARC does not automate malloc/free, management of the lifetime of Core Foundation objects, file descriptors, and so on, you still free such resources by writing a dealloc method.
+
+//You do not have to (indeed cannot) release instance variables, but you may need to invoke [self setDelegate:nil] on system classes and other code that isn’t compiled using ARC.
+
+//dealloc methods in ARC do not require—or allow—a call to [super dealloc]; the chaining to super is handled and enforced by the runtime.
+
 -(void)dealloc
 {
     
@@ -38,5 +45,7 @@
     *error = [[NSError alloc] initWithDomain:@"dennycd.me" code:1 userInfo:nil];
     return YES;
 }
+
+
 
 @end
