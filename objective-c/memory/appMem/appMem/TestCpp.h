4711174247ca0287b98b3a6200eb0b268fbd2ad4
Denny C. Dai <dennycd@me.com>
2013-08-19 13:59:27 -0700

    objc obj reference from C++

--- objective-c/memory/appMem/appMem/TestCpp.h
+++ objective-c/memory/appMem/appMem/TestCpp.h
@@ -12,16 +12,16 @@
 #include <iostream>
 
 /**
- You cannot use object pointers in C structures.
+ You cannot use object pointers in C structures. / but can do it in a struct within a C++ compilation unit
  Rather than using a struct, you can create an Objective-C class to manage the data instead.
  **/
-//struct TestStruct{
-//    id _obj;
-//};
+struct TestStruct{
+    id _obj;
+};
 
 
 //but u can do it in cpp class
-class TestObject {
+class TestCppObject {
 private:
     id m_obj;
 };
