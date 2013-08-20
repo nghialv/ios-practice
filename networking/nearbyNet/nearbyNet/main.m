1de549179381e01e53183e17e7160daa76ee1316
Denny C. Dai <dennycd@me.com>
2013-08-19 17:04:23 -0700

    nearby networking testing proj

---
+++ networking/nearbyNet/nearbyNet/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  nearbyNet
+//
+//  Created by Denny C. Dai on 8/19/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "nnAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([nnAppDelegate class]));
+    }
+}
---
+++ networking/nearbyNet/nearbyNet/nnAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  nnAppDelegate.h
+//  nearbyNet
+//
+//  Created by Denny C. Dai on 8/19/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface nnAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
