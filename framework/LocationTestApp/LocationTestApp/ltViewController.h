a73de85f06b3dda421ca3355a1683bb3f1ab1f11
Denny C. Dai <dennycd@me.com>
2013-08-30 15:16:40 -0700

    adding framework test app

---
+++ framework/LocationTestApp/LocationTestApp/ltViewController.h
@@ -0,0 +1,13 @@
+//
+//  ltViewController.h
+//  LocationTestApp
+//
+//  Created by Denny C. Dai on 8/30/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ltViewController : UIViewController
+
+@end
---
+++ framework/LocationTestApp/LocationTestApp/ltViewController.m
@@ -0,0 +1,29 @@
+//
+//  ltViewController.m
+//  LocationTestApp
+//
+//  Created by Denny C. Dai on 8/30/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#import "ltViewController.h"
+
+@interface ltViewController ()
+
+@end
+
+@implementation ltViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
