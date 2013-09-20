26baadadcbaf3851fb67de0245df65e8364d8bce
Denny C. Dai <dennycd@me.com>
2013-09-19 23:12:41 -0700

    adding game kit framework testing project

---
+++ game_kit/GameKiitTest/GameKiitTest/ogAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ogAppDelegate.h
+//  GameKiitTest
+//
+//  Created by Denny C. Dai on 9/19/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ogAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ game_kit/GameKiitTest/GameKiitTest/ogViewController.h
@@ -0,0 +1,13 @@
+//
+//  ogViewController.h
+//  GameKiitTest
+//
+//  Created by Denny C. Dai on 9/19/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ogViewController : UIViewController
+
+@end
