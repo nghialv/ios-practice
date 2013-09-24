73bf94c4a5167bc2a7470b8f9f4731f113beb7ef
Denny C. Dai <dennycd@me.com>
2013-09-23 22:33:59 -0700

    renaming top level directory for location and mapping

---
+++ mapping/google/testGMap/testGMap/APIKey.h
@@ -0,0 +1,18 @@
+//
+//  APIKey.h
+//  testGMap
+//
+//  Created by Denny C. Dai on 8/10/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#ifndef testGMap_APIKey_h
+#define testGMap_APIKey_h
+
+// To use GoogleMapsSDKDemos, please register an APIKey for your application
+// and set it here. Please see the README for more information. Your APIKey
+// should be kept private.
+const static NSString *APIKey = @"AIzaSyDzF5-alnqQXUDWgiT5L_b4DL7FKktrByI";
+
+
+#endif
