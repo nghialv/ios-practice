6acfe2b6d5c346f516f7e548a94850cb47ea34a2
Denny C. Dai <dennycd@me.com>
2013-09-24 21:48:52 -0700

    NEW: adding new project for javascript core testing for iOS

---
+++ javascript/jsTest/jsTest/jtAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  jtAppDelegate.h
+//  jsTest
+//
+//  Created by Denny C. Dai on 9/24/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface jtAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ javascript/jsTest/jsTest/jtViewController.h
@@ -0,0 +1,13 @@
+//
+//  jtViewController.h
+//  jsTest
+//
+//  Created by Denny C. Dai on 9/24/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface jtViewController : UIViewController
+
+@end
